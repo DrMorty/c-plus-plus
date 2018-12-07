183284eae8c49910551e853fdf279170cda76577
DrMorty <31830532+DrMorty@users.noreply.github.com>
2018-12-07 16:29:36 +0300

    Create main.cpp

---
+++ Hash/main.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <math.h>
+#include <string>
+#include <fstream> 
+#include <iomanip> 
+#include <cstring>
+#include <vector>           
+#include <sstream>          
+#include <algorithm>    
+#include "reservoir.h"
+#include <list>
+#include <iterator>
+
+int main()
+{
+    Storage stor;
+    stor.setHashValue();
+    stor.readDataBase();
+    stor.readBook();
+    return 0;
+}
