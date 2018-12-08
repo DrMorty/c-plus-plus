c7f6748127389baa3f69c164ac107bff49024a0f
DrMorty <31830532+DrMorty@users.noreply.github.com>
2018-12-08 13:16:30 +0300

    Update Hash.h

--- Hash/Hash.h
+++ Hash/Hash.h
@@ -3,14 +3,6 @@
 #define HASH_H_
 #include <string>
 
-std::string itos(int n)
-{
-   const int max_size = std::numeric_limits<int>::digits10 + 1 /*sign*/ + 1 /*0-term*/;
-   char buffer[max_size] = {0};
-   sprintf(buffer, "%d", n);
-   return std::string(buffer);
-}
-
 std::string comp_hash(std::string book_name)
 {
     std::string hash = "";
