6bf2b5cefc45b49b4b5bc26be9dc833fbf261acd
DrMorty <31830532+DrMorty@users.noreply.github.com>
2018-12-09 20:52:56 +0300

    Update and rename stream to stream.h

--- Hash/stream.h
+++ Hash/stream.h
@@ -2,7 +2,7 @@
 #include <string>
 #include <fstream>
 #include "reservoir.h"
-
+#include <iterator>
 
 
 void readDataBase(std::string filename, Storage* hashTable)
@@ -12,22 +12,23 @@ void readDataBase(std::string filename, Storage* hashTable)
     {
         std::string str;
         std::getline(DATABASE, str);
-        if (str != "") //
+        if (str != "") 
         {
             hashTable->add(str);
         }
     }
 }
 
-void saveDataBase(std::string filename, Storage* hashTable)
+template<typename Type>
+void saveDataBase(std::string filename, Storage<Type>* hashTable)
 {
     std::ofstream DATABASE(filename);
-    std::list<Book>::iterator iter_1;
     for (int i = 0; i < hashTable->cell_count; i++ )
     {
-        for (iter_1 = hashTable->storage[i].begin() ;iter_1 != hashTable->storage[i].end(); iter_1++ )
+        for (auto it : *hashTable->storage[i])
         {
-            DATABASE << iter_1->data << std::endl;
+            DATABASE << it << std::endl;
+        
         }
     }
 }
